507ddc9460859022e57fc6586b33cab74065816f
Parfenchenko <parfencenkoartem@gmail.com>
2021-07-27 17:06:51 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -5,9 +5,40 @@ namespace testTask
 {
     class Program
     {
-        static void Main(string[] args)
-        {
 
+        static int[] ArrGenerator(int[] arr)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 20);
+            }
+
+            return arr;
+        }
+
+        static Dictionary<int, int> OccurrenceOfNubers(int[]  arr)
+        {
+            var obj = new Dictionary<int, int>();
+
+
+            foreach (var i in arr)
+            {
+                int res;
+                if (obj.TryGetValue(i, out res))
+                    obj[i] += 1;
+                else
+                    obj.Add(i, 1);
+            }
+
+            return obj;
+        }
+
+        static void Main(string[] args)
+
+        {
+            //user's input 
             Console.Write("Введите длину массива чисел: ");
 
             var len = Console.ReadLine();
@@ -28,27 +59,12 @@ namespace testTask
                     }
                 }
 
+            //finding occurrence of numbers in the array
             int[] arr;
-
             arr = new int[a];
 
-            Random rnd = new Random();
-
-            for (int i = 0; i < arr.Length ; i++)
-            {
-                arr[i] = rnd.Next(1,20);
-            }           
-
-            var obj = new Dictionary<int, int>();
-
-            foreach (var i in arr)
-            {
-               int res;
-                if (obj.TryGetValue(i, out res))
-                    obj[i] += 1;
-                else
-                    obj.Add(i, 1);
-            }
+            arr = ArrGenerator(arr);
+            var obj = OccurrenceOfNubers(arr);
 
             //output into console
             Console.Write("Исходный массив: ");
